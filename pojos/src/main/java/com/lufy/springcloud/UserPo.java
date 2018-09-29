ff2159d94813f1b3741fc45e0db308d2e02a4bff
lufyluo <lufyl@163.com>
2018-09-29 11:18:52 +0800

    1

---
+++ pojos/src/main/java/com/lufy/springcloud/UserPo.java
@@ -0,0 +1,22 @@
+package com.lufy.springcloud;
+
+public class UserPo {
+    public String name;
+    private String server;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getServer() {
+        return server;
+    }
+
+    public void setServer(String server) {
+        this.server = server;
+    }
+}
--- user1/src/main/java/com/lufy/springcloud/user1/controllers/UserController.java
+++ user1/src/main/java/com/lufy/springcloud/user1/controllers/UserController.java
@@ -1,5 +1,20 @@
 package com.lufy.springcloud.user1.controllers;
 
-public class UserController {
+import com.lufy.springcloud.UserPo;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+//    @Autowired
+//    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/user/{id}")
+    public UserPo getUser(@PathVariable("id") long id) {
+        UserPo userPo = new UserPo();
+        userPo.setName("name is : "+id);
+        return userPo;
+    }
 }
